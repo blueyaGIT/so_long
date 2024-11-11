dfdd56387848bdc532098a524d7f4294bbb891eb
Davide Albano <dalbano@3-F-12.42heilbronn.de>
2024-11-11 12:45:50 +0100

    added first .c and so_long.h

---
+++ so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 12:43:15 by dalbano           #+#    #+#             */
+/*   Updated: 2024/11/11 12:43:56 by dalbano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "libft/libft.h"
+# include "printf/ft_printf.h"
+# include <stdlib.h>
+
+// Implementations of ... operations
+
+// Helper functions
+
+#endif /* SO_LONG_H */
